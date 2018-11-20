776071f7d6f1c594e53a8551a44324ddd1bffbb7
Vladimir Kotal <vlada@devnull.cz>
2018-11-21 00:32:50 +0100

    fix the test

--- src/test/java/org/foo/bar/CoolTest.java
+++ src/test/java/org/foo/bar/CoolTest.java
@@ -1,10 +1,14 @@
 package org.foo.bar;
 
-class CoolTest {
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CoolTest {
     @Test
     public void testFactor() {
         Cool c = new Cool();
         c.setFactor(1);
-        assertEquals(1, getFactor());
+        assertEquals(1, c.getFactor());
     }
 }
